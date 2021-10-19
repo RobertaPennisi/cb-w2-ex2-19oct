1015ea6e52b04e17157924615d096da87f54081f
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-19 18:06:32 +0200

    inserito ternario.js

---
+++ js/ternario.js
@@ -0,0 +1,10 @@
+let animal = prompt("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda")
+
+let output = (animal==1) ? "Choose the Dog" :
+ (animal==2) ? "Choose the Cat" : 
+ (animal==3) ? "Choose the Goldfish" : 
+ (animal==4) ? "Choose the Monkey" : 
+ (animal==5) ? "Choose the Panda" :
+ "Animal not found"
+
+console.log(output);
